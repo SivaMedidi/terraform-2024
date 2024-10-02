3cfa846c53e18687bfed18abff8236acf3658f5b
IshaqMd <55760349+ishaq-md@users.noreply.github.com>
2024-10-02 21:33:35 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "google" {
+  project     = "mi-cloud-dev"
+  region      = "us-central1"
+  credentials = file("terraform.json")
+
+}
+
+resource "google_compute_instance" "default" {
+  name         = "my-vm"
+  machine_type = "e2-medium"
+  zone         = "us-central1-f"
+  tags = ["hr", "dev"]
+
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
